91b8bf8310d7fd62fe6252362b4d5e3db2615e00
staszko <30053337+staszko@users.noreply.github.com>
2021-05-02 14:34:47 +0200

    Spring init project from start.spring.io

---
+++ src/main/java/pl/org/staszko/chucknorrisjokesspringudemy/ChuckNorrisJokesSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package pl.org.staszko.chucknorrisjokesspringudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesSpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokesSpringUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/org/staszko/chucknorrisjokesspringudemy/ChuckNorrisJokesSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.org.staszko.chucknorrisjokesspringudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
